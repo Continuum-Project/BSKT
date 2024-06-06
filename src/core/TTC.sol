32880cf65966211122527ce1b85134b338afbf98
Ruslan Akhtariev <rakhtariev@icloud.com>
2024-06-06 13:50:20 -0700

    modifiable ttc names symbols

--- src/core/TTC.sol
+++ src/core/TTC.sol
@@ -9,7 +9,10 @@ import "./TTCConstants.sol";
 import {console} from "forge-std/Test.sol";
 
 contract TTC is ERC20, TTCConstants, Ownable {
-    constructor() ERC20(TTC_NAME, TTC_SYMBOL) Ownable(msg.sender) {}
+    constructor(
+        string memory TTC_NAME,
+        string memory TTC_SYMBOL
+    ) ERC20(TTC_NAME, TTC_SYMBOL) Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
